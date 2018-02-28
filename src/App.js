c2188e23e764569c6fb4bbfa11cd14782cefa044
Bill Risher <bill.risher99@gmail.com>
2018-02-28 09:27:46 -0600

    made basic app function
    App does what it's supposed to now

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+let animals = require("./animal-list.json");
+let verb = require("./verb-list.json");
+
+let randomFromList = (list) => {
+  return Math.floor(Math.random() * list.length);
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>A(n) {animals[randomFromList(animals)]} that likes to {verb.verbs[randomFromList(verb.verbs)].present}.</p>
       </div>
     );
   }
