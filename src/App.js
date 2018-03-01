4939961a2ddbcdaf5b855b38ec85c27b6e257152
Bill Risher <bill.risher99@gmail.com>
2018-03-01 15:45:51 -0600

    added stylings
    Added CSS to the program

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 let animals = require("./animal-list.json");
 let verb = require("./verb-list.json");
 
-let randomFromList = (list) => {
-  return Math.floor(Math.random() * list.length);
-}
-
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      animalNum: 0,
+      verbNum: 0
+    };
+    this.handleClick = this.handleClick.bind(this); 
   }
-  
+  handleClick() {
+    let randomFromList = (list) => {
+      return Math.floor(Math.random() * list.length);
+    } 
+    this.setState({animalNum: randomFromList(animals),
+                  verbNum: randomFromList(verb.verbs)});
+  }
   render() {
     return (
       <div className="App">
-        <p>A(n) {animals[randomFromList(animals)]} that likes to {verb.verbs[randomFromList(verb.verbs)].present}.</p>
+        <p className="Text">A <span className="Generated">{animals[this.state.animalNum]}</span> that likes to <span className="Generated">{verb.verbs[this.state.verbNum].present}</span>.</p>
+        <button className="Button" onClick={() => this.handleClick()}>Regenerate</button>
       </div>
     );
   }
